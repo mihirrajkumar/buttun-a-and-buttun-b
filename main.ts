58fd0e4ae2a8f2ac21b81a9d738ee67c20134645
mihirrajkumar <77078237+mihirrajkumar@users.noreply.github.com>
2021-01-06 15:29:06 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("D C E C F E D C ", 274)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playMelody("- - - - - - - - ", 120)
+})
+basic.forever(function () {
+	
+})
